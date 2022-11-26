784e5668540eaa3c18ec1abdec78c101d0e39637
Gabriel Castrillon <castrillon.gabriel@gmail.com>
2022-11-25 21:39:20 -0300

    v2.0.20

---
+++ lib/index.ts
@@ -0,0 +1,16 @@
+import { IMCStatus } from './imc/types/imc-status.enum';
+import { MacronutrientsStrategy } from './macronutrients/types/macronutrients-strategy.enum';
+import { PhysicalActivityLevel } from './macronutrients/types/physical-activity-level.enum';
+import { Gender } from './macronutrients/types/gender.enum';
+
+import IMC from './imc/imc';
+import Macronutrients from './macronutrients/macronutrients';
+
+export {
+  IMC,
+  Macronutrients,
+  Gender,
+  IMCStatus,
+  MacronutrientsStrategy,
+  PhysicalActivityLevel,
+};
